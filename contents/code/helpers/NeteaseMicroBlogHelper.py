4c79faa8ca5b51d206043d72f5364ccd3dfa7e76
arthur.kun <arthur.kun@gmail.com>
2011-02-11 09:56:35 +0000

    FIX: NeteaseMicroBlog will open picture in new window

--- contents/code/helpers/NeteaseMicroBlogHelper.py
+++ contents/code/helpers/NeteaseMicroBlogHelper.py
@@ -3,9 +3,24 @@ from PyQt4.QtCore import *
 from PyQt4.QtNetwork import *
 class Helper():
 	def translateUrl(self, originalUrl):
-		if string.find(originalUrl.toEncoded().data(), "http://3g.163.com/t/urlcheck") == 0:
+		urlString = originalUrl.toEncoded().data()
+		if string.find(urlString, "http://3g.163.com/t/urlcheck") == 0:
 			if originalUrl.hasQueryItem(QString("p")):
 				translated = QUrl(originalUrl.queryItemValue(QString("p")))
-				print "NeteaseMicroBlogHelper translated:", originalUrl.toString(), "to", translated
+				print "NeteaseMicroBlogHelper translated shorturl:", originalUrl.toString(), "to", translated
+				return translated
+		if string.find(urlString, "http://3g.163.com/post/pic_xhtml.jsp") == 0:
+			if originalUrl.hasQueryItem(QString("picurl")):
+				translated = QUrl(originalUrl.queryItemValue(QString("picurl")))
+				if translated.hasQueryItem(QString("url")):
+					translated = QUrl(translated.queryItemValue(QString("url")))
+				print "NeteaseMicroBlogHelper translated picture url:", originalUrl.toString(), "to", translated
 				return translated
 		return originalUrl
+	def needNewWindow(self, originalUrl):
+		urlString = originalUrl.toEncoded().data()
+		if string.find(urlString, "http://3g.163.com/t/urlcheck") == 0:
+			return True
+		if string.find(urlString, "http://3g.163.com/post/pic_xhtml.jsp") == 0:
+			return True
+		return False
